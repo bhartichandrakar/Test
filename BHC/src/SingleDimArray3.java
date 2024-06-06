9f17cffc32843d1802276a9fd0a45ec2d7d78737
Bharti Chandrakar <bharti3710@gmail.com>
2024-06-06 20:34:16 +0530

    SingleDimArray3

---
+++ BHC/src/SingleDimArray3.java
@@ -0,0 +1,13 @@
+public class SingleDimArray3 {
+    public static void main(String[]args)
+    {
+        int a[]={100,200,300,400,500,600};
+        System.out.println("Array Length:"+a.length);
+        
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+    
+}
