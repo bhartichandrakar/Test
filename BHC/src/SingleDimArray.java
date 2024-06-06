37573f441c8bb897c05f19de00a307be51139507
Bharti Chandrakar <bharti3710@gmail.com>
2024-06-06 20:18:09 +0530

    singledimarray

---
+++ BHC/src/SingleDimArray.java
@@ -0,0 +1,13 @@
+public class SingleDimArray {
+    public static void main(String[]args)
+    {
+        int a[]=new int[5];
+        a[0]=100;
+        a[1]=200;
+        a[2]=300;
+        a[3]=400;
+        a[4]=500;
+        System.out.println("Length of an array is:"+a.length); 
+    }
+    
+}
