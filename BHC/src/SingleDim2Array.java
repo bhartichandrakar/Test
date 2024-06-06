c501bed82ef327b9179859effab6e134e92313e1
Bharti Chandrakar <bharti3710@gmail.com>
2024-06-06 20:21:49 +0530

    array2

---
+++ BHC/src/SingleDim2Array.java
@@ -0,0 +1,8 @@
+public class SingleDim2Array {
+    public static void main(String[]args)
+    {
+        int a[]={100,200,300,400};
+        System.out.print("Array Length:"+a.length);
+    }
+    
+}
