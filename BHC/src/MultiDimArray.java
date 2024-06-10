8ef897b057219969c9982ec2c8e13f2170bb7405
Bharti Chandrakar <bharti3710@gmail.com>
2024-06-10 14:36:18 +0530

    multidimarray1

---
+++ BHC/src/MultiDimArray.java
@@ -0,0 +1,15 @@
+public static void main(String[]args) {
+
+    int a[][]=new int [3][2];
+
+    a[0][0]=11;
+    a[0][1]=13;
+    a[1][0]=15;
+    a[1][1]=17;
+    a[2][0]=19;
+    a[2][1]=21;
+     
+    System.out.println("No. of rows:"+a.length);
+    System.out.println("No. of column:"+a[0].length);
+    
+}
