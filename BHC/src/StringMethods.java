89de08e8ade659b0dd69ebd03b782c8794d9da99
Bharti Chandrakar <bharti3710@gmail.com>
2024-06-10 14:51:13 +0530

    Welcomwtojava

---
+++ BHC/src/StringMethods.java
@@ -0,0 +1,11 @@
+public class StringMethods {
+    public static void main(String[]args){
+        String s="Welcome";
+        String s1="To Java";
+
+        System.out.println(s.length());
+        System.out.println(s.concat(s1));
+    }
+
+    
+}
