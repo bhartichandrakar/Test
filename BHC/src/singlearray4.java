a5d86c2361b9ad4baee2c76f86e929b9bebd4b9f
Bharti Chandrakar <bharti3710@gmail.com>
2024-06-10 14:10:52 +0530

    newchange

---
+++ BHC/src/singlearray4.java
@@ -0,0 +1,11 @@
+public class singlearray4 {
+    public static void main(String[]args)
+    {
+        int a[]={10,20,30,40,50};
+        for(int i:a)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
